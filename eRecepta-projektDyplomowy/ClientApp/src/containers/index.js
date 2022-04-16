2484e5d607f76a55a2371995055d6a56c2da4d95
Marek Winiarski <mm1.winiarski@gmail.com>
2022-04-16 20:36:33 +0200

    Add Personel Section

--- eRecepta-projektDyplomowy/ClientApp/src/containers/index.js
+++ eRecepta-projektDyplomowy/ClientApp/src/containers/index.js
@@ -6,3 +6,5 @@ export { default as Possibility } from './possibility/Possibility';
 export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
 export { default as Recepta } from './recepta/Recepta';
 export { default as Konsultacje } from './konsultacje/Konsultacje';
+export { default as Personel } from './personel/Personel';
+export { default as Kontakt  } from './kontakt/Kontakt';
