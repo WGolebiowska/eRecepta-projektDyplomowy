c889c645f95ff64de44f8ab5a08c5fd8755b1ca4
Alina <wronkaa@gmail.com>
2022-04-21 01:47:37 +0200

    Relacje

---
+++ eRecepta-projektDyplomowy/Models/MedicineIllness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eRecepta_projektDyplomowy.Models
+{
+    public class MedicineIllness
+    {
+        [Key]
+        public int MedicineIllnessId { get; set; }
+
+        public virtual List<Prescription> Prescription { get; set; }
+        public virtual List<Medicine> Medicine { get; set; }
+        public virtual List<Illness> Illness { get; set; }
+    }
+}
